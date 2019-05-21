a36f50cecf0f7f5e36438be51736181cea5cbd43
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-21 17:09:13 +0200

    new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake posts...'
+100.times do
+  articles = Article.new(
+    title: Faker::Company.name,
+    content: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+  )
+  articles.save!
+end
